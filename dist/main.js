01093d8ffe550d9fa2abe110d11fde47d17991e0
kayak <kayak@kayak.mail>
2025-04-03 23:00:50 +0800

    创建项目

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// src/main.ts
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+greet("World");
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// src/main.ts
+function greet(name: string): void {
+    console.log(`Hello, ${name}!`);
+}
+
+greet("World");
